b1dd29b4d8a841e92d5d8b9b2547293b6a8cf4f2
FrancescoCesari97 <freedus97@gmail.com>
2024-02-07 10:04:08 +0100

    completamento milestone

--- main.js
+++ main.js
@@ -3,22 +3,26 @@ const { createApp } = Vue;
 const app = createApp({
   data() {
     return {
-      randomNumber: null,
+      randomEmails: [],
+
+      maxEmail: 10,
     };
   },
 
   methods: {
-    printHello() {
-      console.log("Hello Vue!");
-    },
+    printHello() {},
   },
 
   created() {
-    axios
-      .get("https://flynn.boolean.careers/exercises/api/random/mail")
-      .then((email) => {
-        console.log(email);
-      });
+    for (let i = 0; i < 10; i++) {
+      axios
+        .get("https://flynn.boolean.careers/exercises/api/random/mail")
+        .then((email) => {
+          const mail = email.data.response;
+
+          this.randomEmails.push(mail);
+        });
+    }
   },
 });
 
